dbfd0743b540c1a736a2c0813b9363b09f7e98d8
ABujalance <abujalance@deloittedigital.es>
2022-11-04 18:08:31 +0100

    Routing in React

--- react-course/src/App.jsx
+++ react-course/src/App.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import "./global.css";
-import { AppContext } from "./App-context";
-import { HelloCheckbox } from "./components/hello-checkbox";
-import { HelloForm } from "./components/hello-form";
-import { HelloCounter } from "./components/hello-counter";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HelloList } from "./components/hello-list";
+import { HelloWorld } from "./components/hello-world";
 export function App() {
   return (
-    <AppContext>
-      <HelloCheckbox />
-      <HelloForm />
-      <HelloCounter />
-    </AppContext>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/list">List</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/list" element={<HelloList />} />
+          <Route path="/" element={<HelloWorld />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
